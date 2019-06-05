5c86db2077d9a1342cc5312fecdd0bf4fcde209f
mentab <25363363+mentab@users.noreply.github.com>
2019-06-05 13:08:27 +0200

    Updates

---
+++ precache-manifest.b33f7942affccc2814e6687072e3d9ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "5b88ff49f309f05e183e",
+    "url": "/drum-machine/static/js/main.b8681d26.chunk.js"
+  },
+  {
+    "revision": "abd611d99f8baa7aa992",
+    "url": "/drum-machine/static/js/2.465c357c.chunk.js"
+  },
+  {
+    "revision": "abd611d99f8baa7aa992",
+    "url": "/drum-machine/static/css/2.42a8a8d3.chunk.css"
+  },
+  {
+    "revision": "7a8f9bb3afca72d15dfb82304767a195",
+    "url": "/drum-machine/index.html"
+  }
+];
